f271c416ba6749dd729f644de6644a3fa4c30218
christafayereyes <christafayereyes@gmail.com>
2022-04-08 11:25:03 -0500

    Changed radio button value

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -14,7 +14,7 @@ function Checkout() {
             <div className="shopping-cart card mb-3 m-5 w-50 border border-primary rounded">
                 {items.map((item) =>
                     <div class="form-check border-bottom border-primary">
-                        <input class="form-check-input mt-5 ms-3" type="checkbox" name="exampleRadios" value="option1" checked/>
+                        <input class="form-check-input mt-5 ms-3" type="checkbox" value="" checked/>
                         <div className="row g-0">
                             <div key={uuid()} className="col-md-4" >
                                 <img src={item.src} alt="" className="img-fluid rounded-start m-3 ms-5"/>
